84cb1f0ce6d6c829e2777e35b1be54924adcabf9
rafinhadhc-dot <rafinhadhc@gmail.com>
2026-03-16 20:00:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const dataFesta = new Date("March 25, 2026 20:00:00").getTime();
+
+const contador = document.getElementById("contador");
+
+setInterval(function(){
+
+const agora = new Date().getTime();
+const diferenca = dataFesta - agora;
+
+const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+const horas = Math.floor((diferenca % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const minutos = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60));
+
+contador.innerHTML =
+`Faltam ${dias} dias, ${horas} horas e ${minutos} minutos para a festa 🎉`;
+
+},1000);
